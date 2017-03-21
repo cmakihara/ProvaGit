5b8413f65416c9da6dc7625cfbcee0477d603a71
clmakihara <clmakihara@gmail.com>
2017-03-21 01:17:49 -0300

    oi

---
+++ Prova02/src/br/univel/Principal.java
@@ -0,0 +1,26 @@
+package br.univel;
+
+import javax.swing.JFrame;
+
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		JFrame jf = new JFrame();
+		
+		jf.setSize(600, 600);
+		jf.setLocationRelativeTo(null);
+		
+		Exe02 meuPainel = new Exe02();
+		
+		jf.setContentPane(meuPainel);
+		//
+		
+		
+		jf.setVisible(true);
+		
+	}
+
+}
